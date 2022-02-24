5b4cfc395eae5ff31c7496cf11b095839fd6fd24
Nestor Pineda <nestorpinedaelvira@gmail.com>
2022-02-24 10:36:12 +0100

    Introduction part almost  ready

---
+++ src/components/Introduction.jsx
@@ -0,0 +1,27 @@
+import "../scss/components/_introduction.scss";
+
+const Introduction = () => {
+  return (
+    <div className="introduction">
+      <div className="introduction__left"></div>
+      <div className="introduction__center">
+        <div className="introduction__col">
+          <h3 className="intro-title">The Restaurant</h3>
+          <img className="intro-img" src="images/gallery/gallery-1.jpg" alt="introduction" />
+        </div>
+        <div className="introduction__col">
+          <h3 className="intro-title">The Story</h3>
+          <p className="intro-paragraph">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis provident, reiciendis ullam accusamus, adipisci accusantium repellat, sint modi ipsam reprehenderit error! Soluta, officiis! Odit facere temporibus officiis hic iusto id!
+            <br />
+            <br />
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis provident, reiciendis ullam accusamus, adipisci accusantium repellat, sint modi ipsam reprehenderit error! Soluta, officiis! Odit facere temporibus officiis hic iusto id!
+          </p>
+        </div>
+      </div>
+      <div className="introduction__right"></div>
+    </div>
+  );
+};
+
+export default Introduction;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,11 +2,13 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Title from "./Title";
 import ImageGallery from "./ImageGallery";
+import Introduction from "./Introduction";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
+      <Introduction />
       <Title />
       <ImageGallery />
       <div>{children}</div>
