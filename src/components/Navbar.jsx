d1c9697d187b023ac132a570567703ba46b1c1b6
Nestor Pineda <nestorpinedaelvira@gmail.com>
2022-02-25 10:57:12 +0100

    navigation styles ready

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,47 +3,48 @@ import Navbar from "react-bootstrap/Navbar";
 import { Container, Form } from "react-bootstrap";
 import { Offcanvas } from "react-bootstrap";
 import { Nav } from "react-bootstrap";
-import { NavDropdown } from "react-bootstrap";
-import { FormControl } from "react-bootstrap";
-import { Button } from "react-bootstrap";
 
 import "../scss/components/_navigation.scss";
 
 const Navigation = () => {
   return (
-    <div className="navigation">
-      <div className="navigation__left"></div>
-      <div className="navigation__center">
-        <Navbar bg="light" expand={false}>
-          <Container fluid>
-            <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
-            <Navbar.Toggle aria-controls="offcanvasNavbar" />
-            <Navbar.Offcanvas id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" placement="end">
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">Home</Nav.Link>
-                  <Nav.Link href="#action2">Link</Nav.Link>
-                  <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
-                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
-                  </NavDropdown>
-                </Nav>
-                <Form className="d-flex">
-                  <FormControl type="search" placeholder="Search" className="me-2" aria-label="Search" />
-                  <Button variant="outline-success">Search</Button>
-                </Form>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
+    <nav>
+      <div className="pre-navigation">
+        <div className="pre-navigation__left"></div>
+        <div className="pre-navigation__center"></div>
+        <div className="pre-navigation__right"></div>
       </div>
-      <div className="navigation__right"></div>
-    </div>
+      <div className="navigation">
+        <div className="navigation__left"></div>
+        <div className="navigation__center">
+          <Navbar expand={false}>
+            <Container fluid>
+              <Navbar.Brand href="#">
+                <img src="images/the-burger-logo.png" alt="The Burger" className="nav-logo" />
+              </Navbar.Brand>
+              <Navbar.Toggle />
+              <Navbar.Offcanvas id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" placement="end">
+                <Offcanvas.Header closeButton>
+                  <Offcanvas.Title id="offcanvasNavbarLabel">
+                    <img src="images/the-burger-logo.png" alt="The Burger" className="nav-offcanvas-logo" />
+                  </Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                  <Nav className="justify-content-end flex-grow-1 pe-3">
+                    <Nav.Link href="#action1">Home</Nav.Link>
+                    <Nav.Link href="#action2">Menu</Nav.Link>
+                    <Nav.Link href="#action2">Register</Nav.Link>
+                    <Nav.Link href="#action2">Login</Nav.Link>
+                    <Nav.Link href="#action2">Logout</Nav.Link>
+                  </Nav>
+                </Offcanvas.Body>
+              </Navbar.Offcanvas>
+            </Container>
+          </Navbar>
+        </div>
+        <div className="navigation__right"></div>
+      </div>
+    </nav>
   );
 };
 
