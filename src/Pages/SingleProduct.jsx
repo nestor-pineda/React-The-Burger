0b7b8e3e7a52c4a478aba1f8ee0b3569e633d1bf
Nestor Pineda <nestorpinedaelvira@gmail.com>
2022-03-10 14:03:08 +0100

    single product working ok

--- src/Pages/SingleProduct.jsx
+++ src/Pages/SingleProduct.jsx
@@ -1,14 +1,36 @@
 import SingleProductInfo from "../Components/SingleProductInfo";
 import UserData from "../Components/UserData";
+import useFetch from "../CustomHooks/useFetch";
+import { useEffect } from "react";
+import { useState } from "react";
 
 import { useParams } from "react-router-dom";
 
 const SingleProduct = () => {
   const { id } = useParams();
+  const [apiData, setApiData] = useState(null);
+
+  // const { apiData, loading, error } = useFetch("http://localhost:8000/carta/1");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/carta/" + id)
+      .then((res) => {
+        return res.json();
+      })
+
+      .then((data) => {
+        setApiData(data);
+        console.log(data);
+      });
+  }, []);
 
   return (
     <>
-      <SingleProductInfo id={id} />
+      {/* {apiData && <p>{apiData.title}</p>} */}
+      {apiData && <SingleProductInfo apiData={apiData} />}
+
+      {/* {loading && <div>Loading...</div>}
+      {error && <div>{error}</div>} */}
       <h1>This is the singel product page with id: {id}</h1>
 
       <h1>Carrousel</h1>
--- src/components/SingleProductInfo.jsx
+++ src/components/SingleProductInfo.jsx
@@ -1,18 +1,18 @@
 import "../scss/components/_single-product-info.scss";
 
-const SingleProductInfo = ({ id }) => {
+const SingleProductInfo = ({ apiData }) => {
   return (
     <div className="single-product">
       <div className="single-product__left"></div>
       <div className="single-product__center">
         <div className="single-product__col single-product__col--border">
-          <img className="single-product__img" src="images/home-3.jpg" alt="hero other page" />
+          <img className="single-product__img" src={`/images/${apiData.image}`} alt="hero other page" />
         </div>
         <div className="single-product__col">
           <div className="single-product__info">
-            <h3 className="single-product__title">Titulo</h3>
-            <h3 className="single-product__price">{id}</h3>
-            <p className="single-product__paragraph">body</p>
+            <h3 className="single-product__title">{apiData.title}</h3>
+            <h3 className="single-product__price">{apiData.price}€</h3>
+            <p className="single-product__paragraph">{apiData.body}</p>
             <h5 className="single-product__subtitle">Allergens</h5>
             <div className="single-product__allergens">
               <img className="single-product__allergen-img" src="images/allergens/egg.png" alt="egg" />
