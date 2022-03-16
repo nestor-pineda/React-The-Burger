49a58b3aa32ca30ecb0a7d9d5ac4f232ff71fa0c
Nestor Pineda <nestorpinedaelvira@gmail.com>
2022-03-16 10:22:54 +0100

    styling loggin page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -23,25 +23,29 @@ function Login(props) {
   };
 
   return (
-    <div className="container">
-      <div className="formContainer">
-        <h1>Login Page</h1>
-        {errorMessage ? <p className="error">{errorMessage}</p> : null}
-        <form>
-          <div className="loginForm">
-            <div className="loginFormItem">
-              <label htmlFor="email">Username</label>
-              <input type="text" id="email" value={email} onChange={(e) => setEmail(e.target.value)} disabled={loading} />
-            </div>
-            <div className="loginFormItem">
-              <label htmlFor="password">Password</label>
-              <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} disabled={loading} />
-            </div>
+    <>
+      <div className="login-section">
+        <div className="login-section__left"></div>
+        <div className="login-section__center">
+          <div className="form-container">
+            <h1 className="form-container__title">Login Page</h1>
+            {errorMessage ? <p className="error">{errorMessage}</p> : null}
+            <form>
+              <div className="login-form">
+                <div className="login-form__item">
+                  <input type="text" id="email" value={email} onChange={(e) => setEmail(e.target.value)} disabled={loading} placeholder="Username" />
+                </div>
+                <div className="login-form__item">
+                  <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} disabled={loading} placeholder="Password" />
+                </div>
+              </div>
+              <button onClick={handleLogin}>login</button>
+            </form>
           </div>
-          <button onClick={handleLogin}>login</button>
-        </form>
+        </div>
+        <div className="login-section__right"></div>
       </div>
-    </div>
+    </>
   );
 }
 
